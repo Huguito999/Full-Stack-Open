365b61d5758a0065ae78ce1df1fa8a2706c667d7
Huguito999 <123948059+Huguito999@users.noreply.github.com>
2024-06-26 17:40:25 +0200

    Courseinfo 2.1.-2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,32 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map((part) =>(
+     <Part key = {part.id} part={part}/>))}    
+  </>
+
+
+const Course = ({course}) => {
+
+const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+console.log(course);
+
+ return (
+  <div>
+    
+    <Header course ={course.name}/>
+    <Content parts={course.parts}/>
+    <Total sum={totalExercises}/>
+
+  </div>
+ )
+}
+export default Course
